8e179946a8546bc1e03449aa5daac85b929902e0
Maxim Makovey <maxim@makovey.net>
2016-12-06 20:03:25 -0800

    added sensehat integration plus a little red dot movement

--- gameofpi.py
+++ gameofpi.py
@@ -1,13 +1,78 @@
 from flask import Flask, render_template
+from sense_hat import SenseHat
+sense=SenseHat()
+
+def color(c,d):
+    global sense
+    sense.set_pixel(c,d,255,0,0)
+
+def clear(c,d):
+    global sense
+    sense.set_pixel(c,d,0,0,0)
+def blank():
+    for x in range(8):
+        for y in range(8):
+            clear(x, y)
+
 app = Flask(__name__)
+x=4
+y=4
+
 
 @app.route("/")
 def index():
     return "Hello World!"
 
+@app.route('/hello')
 @app.route('/hello/<name>')
 def hello(name=None):
+    if name:
+        sense.show_message(name)
     return render_template('hello.html', name=name)
+@app.route('/map')
+def mapping():
+    return render_template('map.html')
+
+@app.route("/up")
+def up():
+    global x,y
+    clear(x,y)
+    y = y+1
+    if y>7:
+        y=0
+    color(x,y)
+    return "It's Up"
+
+@app.route("/down")
+def down():
+    global x,y
+    clear(x,y)
+    y = y-1
+    if y<0:
+        y=7
+    color(x,y)
+    return "It's Down"
+
+@app.route("/left")
+def left():
+    global x,y
+    clear(x,y)
+    x=x-1
+    if x<0:
+        x=7
+    color(x,y)
+    return "It's gone left"
+
+@app.route("/right")
+def right():
+    global x,y
+    clear(x,y)
+    x=x+1
+    if x>7:
+        x=0
+    color(x,y)
+    return "It's gone the right way"
 
 if __name__ == "__main__":
+    blank()
     app.run()
